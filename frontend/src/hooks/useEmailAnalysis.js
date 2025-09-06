76562e5a862651f49a3d1165d97cb3af94c83191
Yasmin Couto <yas.matosc@gmail.com>
2025-09-06 12:04:54 -0300

    commit alterando base url frontend

--- frontend/src/hooks/useEmailAnalysis.js
+++ frontend/src/hooks/useEmailAnalysis.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:5000";
+//const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export default function useEmailAnalysis() {
   const [loading, setLoading] = useState(false);
